100624ffdfe073c25f7ff8804a7eba720ef83feb
kastov <kastov@gog.sh>
2024-11-16 01:02:24 +0300

    fix: handler

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import { createChannel, Channel } from 'nice-grpc';
 import { StatsService } from './src/stats/stats.service';
+import { HandlerService } from './src/handler/handler.service';
 
 export class XtlsApi {
     public readonly channel: Channel;
     public readonly stats: StatsService;
+    public readonly handler: HandlerService;
     constructor(ip: string, port: string) {
         this.channel = createChannel(`${ip}:${port}`);
         this.stats = new StatsService(this.channel);
+        this.handler = new HandlerService(this.channel);
         return this;
     }
 }
